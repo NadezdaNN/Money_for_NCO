bbd7435d651d6be3db4fd3a002fb73c8c667e0fd
NadezdaNN <94559641+NadezdaNN@users.noreply.github.com>
2024-02-27 17:18:06 +0300

    Add files via upload

---
+++ Part3.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, json
+#from contextlib import contextmanager
+#from flask import template_rendered
+from concurrent.futures import ThreadPoolExecutor
+import webbrowser
+from myPredict import*
+
+app = Flask(__name__, static_folder="static")
+
+
+@app.route('/get_proba', methods=['GET', 'POST'])
+def get_len():
+    region = request.form.get('region') # запрос к данным формы
+    opf = request.form.get('opf')
+    okved = request.form.get('okved')
+    addOkved = request.form.get('addOkved')
+    age = request.form.get('age')
+    proba = func_predict(region, opf, okved, addOkved, age)  
+    
+    return json.dumps({'proba': proba})
+
+  
+@app.route('/predict', methods=['GET','POST'])
+def predict():                
+    return render_template('predict.html') 
+
+
+if __name__ == "__main__":       
+    webbrowser.open_new("http://127.0.0.1:5000/predict")
+    app.run() 
